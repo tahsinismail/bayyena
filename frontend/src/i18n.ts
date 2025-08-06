df1ffce3f33abea63778a73dbc9b7d4c40dbb0c4
Tahsin Ismail <tahsinismail.dev@gmail.com>
2025-08-06 11:49:53 +0300

    Language feature for EN and AR with i18n

---
+++ frontend/src/i18n.ts
@@ -0,0 +1,32 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import translationEN from './locales/en/translation.json';
+import translationAR from './locales/ar/translation.json';
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  ar: {
+    translation: translationAR,
+  },
+};
+
+i18n
+  .use(LanguageDetector) // Detect user language
+  .use(initReactI18next) // Pass i18n down to react-i18next
+  .init({
+    resources,
+    fallbackLng: 'en', // Use English if detected language is not available
+    interpolation: {
+      escapeValue: false, // React already safes from xss
+    },
+    detection: {
+      order: ['localStorage', 'navigator'], // Check localStorage first, then the browser
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 import { AuthProvider } from './contexts/AuthContext.tsx';
+import './i18n'; // <-- IMPORT THIS FILE HERE
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
